b66fb5521317d1df1510cc2ff06c205fb587d9dc
Jared Forsyth <jared@jaredforsyth.com>
2017-07-05 21:37:21 -0600

    add npm install

--- source/_posts/run.sh
+++ source/_posts/run.sh
@@ -1,3 +1,3 @@
 set -e
-node ~/clone/experiments/explorable-code/js/notablemind/convertNm.js a-reason-react-tutorial.nm a-reason-react-tutorial.html a-reason-react-tutorial.sandbox.json /assets/a-reason-react-tutorial
+node ~/clone/experiments/explorable-code/js/notablemind/convertNm.js a-reason-react-tutorial.nm a-reason-react-tutorial.html a-reason-react-tutorial.sandbox.json /assets/a-reason-react-tutorial "2017-07-05 20:06:00"
 node ~/clone/experiments/explorable-code/js/executeSandbox.js a-reason-react-tutorial.sandbox.json ../../playgrounds/a-reason-react-tutorial ../assets/a-reason-react-tutorial
